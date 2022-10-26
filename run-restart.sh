b9c27cce1d27e6b3313e009747f1538ff8ac6588
WhenSunset <a1018998632@gmail.com>
2022-10-26 12:39:25 +0800

    V2

--- run-restart.sh
+++ run-restart.sh
@@ -1,14 +1,5 @@
-cd pyspider
-sudo sh run.sh
-
 cd ../ParseServer
 sudo sh run.sh
 
 cd ../MetaBase
-sudo sh run.sh
-
-cd ../n8n
-sudo sh run.sh
-
-cd ../selenium
 sudo sh run.sh
--- run.sh
+++ run.sh
@@ -1,15 +1,6 @@
-cd pyspider
-sudo sh run.sh
-
 cd ../ParseServer
 sudo sh run.sh
 
 cd ../MetaBase
 sudo rm -rf data/db
-sudo sh run.sh
-
-cd ../n8n
-sudo sh run.sh
-
-cd ../selenium
 sudo sh run.sh
